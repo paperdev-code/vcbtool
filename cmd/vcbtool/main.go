9eac19d427b6101745f1101a3990c8112aded242
paperdev <jornveken2@gmail.com>
2022-05-26 20:33:20 +0200

    fix typo

--- cmd/vcbtool/main.go
+++ cmd/vcbtool/main.go
@@ -21,14 +21,14 @@ func main() {
 	}
 
 	fmt.Print("Circuit:")
-	for i := 0; i < len(cc.Logic_layer); i += 1 {
+	for i := 0; i < len(cc.Layer); i += 1 {
 		if i%int(cc.Width) == 0 {
 			fmt.Print("\n")
 		}
-		rgba := vcb.InkColors[cc.Logic_layer[i]]
+		rgba := vcb.InkColors[cc.Layer[i]]
 		color_bg := fmt.Sprintf("\x1b[48;2;%d;%d;%dm", rgba.R, rgba.G, rgba.B)
 		color_fg := fmt.Sprintf("\x1b[38;2;%d;%d;%dm", rgba.R/2, rgba.G/2, rgba.B/2)
-		fmt.Printf("%s%s%.2d\x1b[0m", color_bg, color_fg, cc.Logic_layer[i])
+		fmt.Printf("%s%s%.2d\x1b[0m", color_bg, color_fg, cc.Layer[i])
 	}
 	fmt.Print("\n")
 }
